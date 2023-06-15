ea661eb2237fcf478b29bd293ca3b495cd0e11ca
JCOMET23 <gabecometa@gmail.com>
2023-06-15 13:28:13 -0700

    puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { puppyList } from './data'
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
-    <>
-
-    </>
+    <div className = 'App'>
+      { 
+        puppies.map((puppy) => {
+          return <p key = {puppy.id}>{puppy.name}</p>
+        })
+      }
+    </div>
   )
 }
 
