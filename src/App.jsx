7f3aace27979b73eeced8124cba9ec72b8554661
JCOMET23 <gabecometa@gmail.com>
2023-06-15 16:34:45 -0700

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
 import { useState } from 'react'
 import './App.css'
+// import './index.css'
 import { puppyList } from './data'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  
+  const handleClick = (id) => {
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
+
   return (
     <div className = 'App'>
       { 
         puppies.map((puppy) => {
-          return <p key = {puppy.id}>{puppy.name}</p>
+          return <p 
+                    onClick={()=>{setFeatPupId(puppy.id)}}
+                    key = {puppy.id}>{puppy.name}
+                  </p>
         })
       }
+      <h2>Featured:</h2>
+      {featPupId && (
+        <div className = 'featured-pup'>
+          <h4>{featuredPup.name}</h4>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   
   )
